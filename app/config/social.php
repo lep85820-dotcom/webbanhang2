cdf7d0d81f2029badfeabd64f5939fa400f281f9
Administrator <admin@webbanhang.local>
2026-06-02 10:33:50 +0700

    Fix Google API keys

--- app/config/social.php
+++ app/config/social.php
@@ -2,8 +2,8 @@
 // Cấu hình thông tin API cho đăng nhập qua mạng xã hội
 // Bạn cần thay thế các giá trị bên dưới bằng Client ID và Client Secret thực tế của bạn
 
-define('GOOGLE_CLIENT_ID', '118318721666-' . '410ohqov4bdf98idmbms6c243n1l7shb.apps.googleusercontent.com');
-define('GOOGLE_CLIENT_SECRET', 'GOCSPX-' . 'qO6Qn3-qG3H45rW1a52h14dG_J3m');
+define('GOOGLE_CLIENT_ID', '494210799484-' . 'mcpqnbli9m1k2el6d5ao7kmunbv85f38.apps.googleusercontent.com');
+define('GOOGLE_CLIENT_SECRET', 'GOCSPX-' . '9StTE4wsj1I9lBIocHZPWO5n4CDW');
 define('GOOGLE_REDIRECT_URI', 'http://localhost:8080/webbanhang/account/googleCallback');
 
 define('GITHUB_CLIENT_ID', 'Ov23li82' . 'F8heM5AUGcfc');
